4e88208675cbf489d3b78fac5a1c575b02d38992
beatopia <kailuzniak@gmail.com>
2026-08-19 05:51:18 -0700

    turned qm into a func. saving progress before adding discord webhook

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pyautogui as pag
 import time
 
-while 1:
-    pixel_color = pag.pixel(950, 20)
-    if pixel_color == (140, 229, 0):
-        print("Match found!", pixel_color)
-    elif pixel_color == (81, 43, 39):
-        print("In queue!", pixel_color)
-    else:
-        print("Something is wrong! OW is not focused or your pixel selection is incorrect.")
-    time.sleep(1)
+
+def queue_monitor():
+    match_found = False
+    while match_found == False:
+        pixel_color = pag.pixel(950, 20)
+        if pixel_color == (140, 229, 0):
+            print("Match found!", pixel_color)
+            match_found = True
+        elif pixel_color == (81, 43, 39):
+            print("In queue!", pixel_color)
+        else:
+            print("Something is wrong! OW is not focused or your pixel selection is incorrect.")
+        time.sleep(1)
+
+
+queue_monitor()
