a9f8c3b641dcd70a539ef8e8815f6f3012c20573
beatopia <kailuzniak@gmail.com>
2026-08-19 05:39:32 -0700

    added pixel detection for in queue vs. match found

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import pyautogui
+import pyautogui as pag
 import time
 
 while 1:
-    pixel_color = pyautogui.pixel(960, 100)
-    print(pixel_color)
+    pixel_color = pag.pixel(950, 20)
+    if pixel_color == (140, 229, 0):
+        print("Match found!", pixel_color)
+    elif pixel_color == (81, 43, 39):
+        print("In queue!", pixel_color)
+    else:
+        print("Something is wrong! OW is not focused or your pixel selection is incorrect.")
     time.sleep(1)
